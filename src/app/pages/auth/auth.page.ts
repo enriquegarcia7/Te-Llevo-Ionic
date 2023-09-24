3cbe371e21f82e75355bb6e525f9f763bbc7674c
Enrique García <enrique7729@gmail.com>
2023-09-24 17:49:01 -0300

    Agregado pass incorrect, cargando, service

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-auth',
@@ -15,15 +16,34 @@ export class AuthPage implements OnInit {
   })
 
   firebaseSvc = inject(FirebaseService);
+  utilsSvc = inject(UtilsService);
 
   ngOnInit() {}
 
-  submit() {
+  async submit() {
     if (this.form.valid) {
+
+      const loading = await this.utilsSvc.loading();
+      await loading.present();
+
       this.firebaseSvc.signIn(this.form.value as User).then( res => {
         
         console.log(res);
         
+      }).catch(error => {
+        console.log(error);
+
+        this.utilsSvc.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        });
+
+
+      }).finally(() => {
+        loading.dismiss();
       })
     }
   }
