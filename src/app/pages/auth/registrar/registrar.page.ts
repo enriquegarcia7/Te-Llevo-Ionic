a3e9286a54ac276267bdf3f95a953e3978918bc5
Enrique García <enrique7729@gmail.com>
2023-09-24 21:23:58 -0300

    se agrega formulario de registro conectado a firebase

--- src/app/pages/auth/registrar/registrar.page.ts
+++ src/app/pages/auth/registrar/registrar.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-registrar',
@@ -7,9 +11,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarPage implements OnInit {
 
-  constructor() { }
+  form = new FormGroup({
+    uid: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+  })
 
-  ngOnInit() {
+  firebaseSvc = inject(FirebaseService);
+  utilsSvc = inject(UtilsService);
+
+  ngOnInit() {}
+
+  async submit() {
+    if (this.form.valid) {
+
+      const loading = await this.utilsSvc.loading();
+      await loading.present();
+
+      this.firebaseSvc.signUp(this.form.value as User).then( async res => {
+
+        await this.firebaseSvc.updateUser(this.form.value.name);
+        console.log(res);
+
+      }).catch(error => {
+        console.log(error);
+
+        this.utilsSvc.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        });
+
+
+      }).finally(() => {
+        loading.dismiss();
+      })
+    }
   }
 
+
+  async setUserInfo(uid: string) {
+    if (this.form.valid) {
+
+      const loading = await this.utilsSvc.loading();
+      await loading.present();
+
+      let path ='users/${uid}';
+      delete this.form.value.password;
+
+
+      this.firebaseSvc.setDocument(path, this.form.value).then( async res => {
+
+        this.utilsSvc.saveInLocalStorage('user', this.form.value)
+        
+      }).catch(error => {
+        console.log(error);
+
+        this.utilsSvc.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        });
+
+
+      }).finally(() => {
+        loading.dismiss();
+      })
+    }
+  }
 }
